08220817856b503b0ffa507c25dbe10272dd8401
Bhattacharya <sumantra.bhattacharya@siemens.com>
2018-08-14 00:32:34 +0530

    Assignment -7.1

---
+++ script.js
@@ -0,0 +1,26 @@
+/* Assigning values to variables as per problem */
+var num1 = -8;
+var num2 = 12.5;
+var num3 = -6.769;
+var num4 = 0;
+var num5 = -1;
+// Introducing new variable, assigning the first number to it
+var max = num1;
+
+if (num2 > max)
+{
+    max = num2;
+}
+if (num3 > max)
+{
+    max = num3;
+}
+if (num4 > max)
+{
+    max = num4;
+}
+if (num5 > max)
+{
+    max = num5;
+}
+console.log("maximum number is : " + max);
